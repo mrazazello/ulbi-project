bf2dd0527d666c2cfa4f57ed46682cd731266083
Konstantin Volkov <azazello@interso.ru>
2023-07-21 16:13:31 +0400

    fix tests exios error

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,31 +1,26 @@
 import path from "path";
 
 export default {
-  clearMocks: true,
   globals: { IS_DEV: true },
-
-  coveragePathIgnorePatterns: ["/node_modules/"],
-
+  clearMocks: true,
+  // The test environment that will be used for testing
+  testEnvironment: "jsdom",
+  coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
+  // An array of file extensions your modules use
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+  // An array of directory names to be searched recursively up from the requiring module's location
+  moduleDirectories: ["node_modules"],
+  modulePaths: ["<rootDir>src"],
+  // The glob patterns Jest uses to detect test files
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  rootDir: "../../",
   setupFilesAfterEnv: ["<rootDir>config/jest/setupTest.ts"],
   moduleNameMapper: {
     "\\.s?css$": "identity-obj-proxy",
     "\\.svg": path.resolve(__dirname, "JestEmptyComponent.tsx"),
+    axios: "axios/dist/node/axios.cjs", // костыль против axios который падает в тестах в ошибку
   },
 
-  // An array of directory names to be searched recursively up from the requiring module's location
-  moduleDirectories: ["node_modules"],
-  rootDir: "../../",
-  modulePaths: ["<rootDir>src"],
-
-  // An array of file extensions your modules use
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
-
-  // The test environment that will be used for testing
-  testEnvironment: "jsdom",
-
-  // The glob patterns Jest uses to detect test files
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
-
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ["/node_modules/"],
+  // testPathIgnorePatterns: ["node_modules/(?!axios)"],
 };
